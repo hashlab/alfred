de120255974b9cf91d6c354b9df65d21485b033c
Christopher Enytc <chris@enytc.com>
2018-01-11 15:55:09 -0200

    Add say script

---
+++ scripts/say.js
@@ -0,0 +1,14 @@
+// Description:
+//   Script for saying anything after you
+//
+// Commands:
+//   hubot say <something> - I will repeat after you
+//
+// Author:
+//   chris@hashlab.com.br
+
+module.exports = function sayScript (robot) {
+  return robot.respond(/say (.*)/i, (res) => {
+    res.send(res.match[1])
+  })
+}
